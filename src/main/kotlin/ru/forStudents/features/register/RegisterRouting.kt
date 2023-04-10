8112c43c2674db40ea935c95e40cd58d017f69c8
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-04-10 23:25:51 +0700

    косметика

--- src/main/kotlin/ru/forStudents/features/register/RegisterRouting.kt
+++ src/main/kotlin/ru/forStudents/features/register/RegisterRouting.kt
@@ -1,14 +1,7 @@
 package ru.forStudents.features.register
 
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import ru.forStudents.features.cache.InMemoryCache
-import ru.forStudents.features.cache.TokenCache
-import ru.forStudents.utils.isValidEmail
-import java.util.*
 
 fun Application.configureRegisterRouting() {
     routing {
