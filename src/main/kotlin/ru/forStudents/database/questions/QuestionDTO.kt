fbdfd42ff304d4f49b82822466a38a4b64b5efc3
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-04-10 16:41:10 +0700

    создана таблица questions

---
+++ src/main/kotlin/ru/forStudents/database/questions/QuestionDTO.kt
@@ -0,0 +1,8 @@
+package ru.forStudents.database.questions
+
+class QuestionDTO(
+    val id : String,
+    val question: String,
+    val topic: String,
+    val userEmail: String
+)
---
+++ src/main/kotlin/ru/forStudents/database/questions/Questions.kt
@@ -0,0 +1,54 @@
+package ru.forStudents.database.questions
+
+import org.apache.logging.log4j.LogManager
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import ru.forStudents.database.users.Users
+
+object Questions : Table("questions") {
+    private val id = Questions.varchar("id", 50)
+    private val topic = Questions.varchar("topic", 250)
+    private val userEmail = Questions.varchar("user_email", 50)
+    private val question = Questions.text("question_body")
+
+    private val logger = LogManager.getLogger(Questions::class.java)
+
+    fun insert(questionDTO: QuestionDTO) {
+        transaction {
+            Questions.insert {
+                it[id] = questionDTO.id
+                it[topic] = questionDTO.topic
+                it[userEmail] = questionDTO.userEmail
+                it[question] = questionDTO.question
+            }
+        }
+    }
+
+    fun fetchQuestions(email: String): List<QuestionDTO>? {
+        return try {
+            transaction {
+                val questionsModel = Questions.select {
+                    Questions.userEmail.eq(email)
+                }.toList()
+                val questions = ArrayList<QuestionDTO>()
+                for (row in questionsModel) {
+                    questions.add(
+                        QuestionDTO(
+                            id = row[Questions.id],
+                            question = row[Questions.question],
+                            topic = row[Questions.topic],
+                            userEmail = row[Questions.userEmail],
+                        )
+                    )
+                }
+                questions
+            }
+        } catch (ex: Exception) {
+            logger.error("fetch questions failed, ${ex.message}")//TODO что с логгированием
+            null
+        }
+    }
+}
+
